b0b1bce7556ed09c4a03eaf71114fa22782d7fae
Manu Pacheco <mpachecopal@gmail.com>
2018-12-16 13:41:22 +0100

    add layout

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-
-  require_once("./database/database.php");
-  require_once("./database/client.php");
-
+  // require_once("./database/database.php");
+  // require_once("./database/client.php");
 ?>
 
 <!DOCTYPE html>
@@ -11,52 +9,48 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <?php include 'partials/css-imports.php'; ?>
   <title>World News</title>
 </head>
 <body>
 
   <?php 
-  
-    $db = new Client($conn);
+    // $db = new Client($conn);
 
-    //CARGA DE PÁGINA DE INICIO
-    $db->loadNews();
+    // //CARGA DE PÁGINA DE INICIO
+    // $db->loadNews();
 
-    //FORMULARIO DE LOGIN
-    $username = htmlspecialchars($POST['username']);
-    $password = htmlspecialchars($POST['password']);
+    // //FORMULARIO DE LOGIN
+    // $username = htmlspecialchars($POST['username']);
+    // $password = htmlspecialchars($POST['password']);
 
-    if(isset($POST['login'])){
-    
-      if(empty($username)){
-        $err = "*";
-      }
-
-      if(empty($password)){
-        $err = "*";
-      }
-
-      if(empty($err)){
-
-        $db->logIn($username, $password);
-
-        if(empty($_SESSION['user'])){
-
-          echo "USUARIO NO ENCONTRADO";
-
-        }
-
-      }
-
-    }
-
-    if(isset($POST['signin'])){
-
-      header("Location: ./signin.php");
-
-    }
-  
+    // if(isset($POST['login'])){
+    //   if(empty($username)){
+    //     $err = "*";
+    //   }
+    //   if(empty($password)){
+    //     $err = "*";
+    //   }
+    //   if(empty($err)){
+    //     $db->logIn($username, $password);
+    //     if(empty($_SESSION['user'])){
+    //       echo "USUARIO NO ENCONTRADO";
+    //     }
+    //   }
+    // }
+    // if(isset($POST['signin'])){
+    //   header("Location: ./signin.php");
+    // }
   ?>
 
+  <main>
+    <?php include 'partials/navbar.php'; ?>
+    <content class="container">
+      <div class="welcome-info">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit corrupti iusto expedita delectus illo obcaecati eaque error, veritatis aliquid et sed accusamus a temporibus, numquam aperiam, nostrum exercitationem facilis culpa.
+      </div>
+    </content>
+  </main>
+  <?php include 'partials/footer.php'; ?>
 </body>
 </html>
---
+++ partials/login-modal.php
@@ -0,0 +1,26 @@
+<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="loginModalLabel">Login</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="inputEmail">Email</label>
+            <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+          </div>
+          <div class="form-group">
+            <label for="inputPassword">Password</label>
+            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+          </div>
+          <!-- <button type="button" class="btn btn-light" data-dismiss="modal">Close</button> -->
+          <button type="submit" class="btn btn-danger">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
